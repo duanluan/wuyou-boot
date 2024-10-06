fe42f426d216f62bed7b3074f38e5ecf73648b4e
duanluan <duanluan@outlook.com>
2024-10-07 01:15:32 +0800

    fix: 根据路由的登录校验无效问题

--- system/src/main/java/top/zhjh/config/satoken/SaTokenConfig.java
+++ system/src/main/java/top/zhjh/config/satoken/SaTokenConfig.java
@@ -1,20 +1,14 @@
 package top.zhjh.config.satoken;
 
 import cn.dev33.satoken.interceptor.SaInterceptor;
-import cn.dev33.satoken.router.SaHttpMethod;
 import cn.dev33.satoken.router.SaRouter;
-import cn.dev33.satoken.router.SaRouterStaff;
 import cn.dev33.satoken.stp.StpUtil;
-import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import top.csaf.lang.StrUtil;
 import top.zhjh.model.entity.SysMenu;
 import top.zhjh.service.SysMenuService;
-
-import java.util.List;
 
 /**
  * Sa-Token 配置
@@ -34,42 +28,16 @@ public class SaTokenConfig implements WebMvcConfigurer {
   public void addInterceptors(InterceptorRegistry registry) {
     // 注册路由拦截器，自定义认证规则
     registry.addInterceptor(new SaInterceptor(handler -> {
-      // 从数据库查询有路径的菜单
-      List<SysMenu> menuList = sysMenuService.list(new LambdaQueryWrapper<SysMenu>().isNotNull(SysMenu::getPath));
-      for (SysMenu menu : menuList) {
-        String path = menu.getPath();
-        if (StrUtil.isBlank(path)) {
-          continue;
-        }
-        String method = menu.getMethod();
-        String permission = menu.getPermission();
+      // 所有路由登录校验
+      SaRouter.match("/**", r -> StpUtil.checkLogin());
 
+      // 循环有路径、权限，需登录的菜单
+      for (SysMenu sysMenu : sysMenuService.lambdaQuery()
+        .isNotNull(SysMenu::getPath)
+        .isNotNull(SysMenu::getPermission)
+        .eq(SysMenu::getNeedToLogin, true).list()) {
         // 校验权限
-        if (StrUtil.isNotBlank(permission)) {
-          SaRouterStaff saRouterStaff = null;
-          if (StrUtil.isNotBlank(method)) {
-            saRouterStaff = SaRouter.match(SaHttpMethod.toEnum(method));
-          }
-          if (saRouterStaff != null) {
-            saRouterStaff = saRouterStaff.match(path);
-          } else {
-            saRouterStaff = SaRouter.match(path);
-          }
-          saRouterStaff.check(r -> StpUtil.checkPermission(menu.getPermission()));
-        }
-        // 登录校验
-        else if (Boolean.TRUE.equals(menu.getNeedToLogin())) {
-          SaRouterStaff saRouterStaff = null;
-          if (StrUtil.isNotBlank(method)) {
-            saRouterStaff = SaRouter.match(SaHttpMethod.toEnum(method));
-          }
-          if (saRouterStaff != null) {
-            saRouterStaff = saRouterStaff.match(path);
-          } else {
-            saRouterStaff = SaRouter.match(path);
-          }
-          saRouterStaff.check(r -> StpUtil.checkLogin());
-        }
+        SaRouter.match(sysMenu.getPath(), r -> StpUtil.checkPermission(sysMenu.getPermission()));
       }
     })).addPathPatterns("/**");
   }
