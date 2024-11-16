3830c4b82bbd1ed0adc8c5a879b81c4a355a4050
duanluan <duanluan@outlook.com>
2024-11-16 21:42:39 +0800

    feat: 菜单树缓存 key 从“多个类型逗号分隔”改为“多个类型逗号分隔_角色编码列表入参/当前用户角色编码列表”

--- system/src/main/java/top/zhjh/model/qo/SysMenuTreeQO.java
+++ system/src/main/java/top/zhjh/model/qo/SysMenuTreeQO.java
@@ -2,7 +2,7 @@ package top.zhjh.model.qo;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
-import top.csaf.lang.StrUtil;
+import top.csaf.coll.CollUtil;
 
 import java.util.List;
 
@@ -11,14 +11,14 @@ import java.util.List;
  */
 @Data
 public class SysMenuTreeQO {
-  @Schema(title = "多个类型：1: 目录, 2: 菜单, 3: 按钮，逗号分隔")
-  private String types;
+  @Schema(title = "类型列表：1: 目录, 2: 菜单, 3: 按钮，逗号分隔")
+  private List<String> types;
   @Schema(title = "角色编码列表")
-  private List<String> roleCodeList;
+  private List<String> roleCodes;
   @Schema(title = "是否获取全部和选中")
   private Boolean isAllAndChecked;
 
   public boolean isNotBlankTypes() {
-    return StrUtil.isNotBlank(types);
+    return CollUtil.isNotEmpty(types);
   }
 }
--- system/src/main/java/top/zhjh/service/SysMenuService.java
+++ system/src/main/java/top/zhjh/service/SysMenuService.java
@@ -23,12 +23,13 @@ public class SysMenuService extends ServiceImpl<SysMenuMapper, SysMenu> {
   @Resource
   private SysMenuMapper sysMenuMapper;
 
-  @Cacheable(value = "menuTree", key = "#query.types", condition = "#query.isNotBlankTypes()")
+  @Cacheable(value = "menuTree", condition = "#query.isNotBlankTypes()",
+    key = "#query.types.toString() + '_' + (!T(top.csaf.coll.CollUtil).isEmpty(#query.roleCodes)  ? #query.roleCodes.toString() : T(cn.dev33.satoken.stp.StpUtil).getRoleList().toString())")
   public List<TreeNode> listTree(SysMenuTreeQO query) {
-    // 如未指定类型
-    if (CollUtil.isEmpty(query.getRoleCodeList())) {
+    // 如未指定角色编码
+    if (CollUtil.isEmpty(query.getRoleCodes())) {
       // 查询当前用户所属编码
-      query.setRoleCodeList(StpUtil.getRoleList());
+      query.setRoleCodes(StpUtil.getRoleList());
     }
     return TreeUtil.build(sysMenuMapper.listTree(query));
   }
