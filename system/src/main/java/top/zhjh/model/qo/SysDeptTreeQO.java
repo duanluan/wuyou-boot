96fc032205bbf47bf81ee54c0b1f522677230700
duanluan <duanluan@outlook.com>
2024-12-08 01:36:47 +0800

    feat: 部门树新增入参：角色编码列表、数据权限操作类型、是否获取全部和选中

--- system/src/main/java/top/zhjh/model/qo/SysDeptTreeQO.java
+++ system/src/main/java/top/zhjh/model/qo/SysDeptTreeQO.java
@@ -3,6 +3,9 @@ package top.zhjh.model.qo;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 import top.zhjh.enums.CommonStatus;
+import top.zhjh.enums.DataScopeActionType;
+
+import java.util.List;
 
 /**
  * 部门树入参
@@ -15,4 +18,11 @@ public class SysDeptTreeQO {
   private String name;
   @Schema(title = "状态")
   private CommonStatus status;
+
+  @Schema(title = "角色编码列表")
+  private List<String> roleCodes;
+  @Schema(title = "数据权限操作类型")
+  private DataScopeActionType dataScopeActionType;
+  @Schema(title = "是否获取全部和选中")
+  private Boolean isAllAndChecked;
 }
